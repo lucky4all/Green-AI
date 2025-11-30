c127a2579c94dd1734ede26e99c1126bc4946bc2
Lucky4all <lucasromerocoscia@gmail.com>
2025-11-30 11:34:08 -0300

    refactor navbar: update navigation links and remove unused code

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,26 @@
+"use client"
 import { raleway } from "@/lib/fonts"
-import Image from "next/image"
+import { useRouter } from "next/navigation"
 export default function NavBar() {
+    const route:any = useRouter()
+    function redirectToHome() {
+        route.push("/")
+    }
+    function redirectToAi() {
+        route.push("/ai")
+    }
+
+    function redirectToAccount() {
+        route.push("/account")
+    }
     return (
         <>
             <nav className="bg-green-500 w-screen h-max py-5">
-                
                 <ul className="flex flex-row items-center justify-between text-white gap-0">
                     <h2 className={`${raleway.className} ml-1 gradient-title text-3xl`}>Green AI |</h2>
-                    <li className="list">Algo</li>
-                    <li className="list">Otro</li>
-                    <li className="list">Sexo</li>
+                    <li onClick={redirectToHome} className="list">Inicio</li>
+                    <li onClick={redirectToAi} className="list">Corregir</li>
+                    <li onClick={redirectToAccount} className="list">Mi cuenta</li>
                 </ul>
             </nav>
         </>
