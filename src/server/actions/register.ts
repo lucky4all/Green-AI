7e6acd922b2dd68976888ec37772162df1ecbebe
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-02 09:17:56 -0300

    created register server action

---
+++ src/server/actions/register.ts
@@ -0,0 +1,20 @@
+"use server"
+import { JWTService } from "@/lib/jwt"
+import { cookies } from "next/headers"
+import sanitizeHtml from 'sanitize-html'
+
+type Payload = {
+    username: string
+}
+
+export default async function registerUser(payload: Payload) {
+    let date = new Date(Date.now() + 1000 * 60 * 60 * 24 * 90) // 90 days
+    let jwt = new JWTService
+
+    const { username } = payload
+    const sanitize = sanitizeHtml(username, { allowedAttributes: {}, allowedTags: [] })
+    payload.username = sanitize
+    const token = jwt.signToken(payload)
+    let cookie = (await cookies()).set("authtoken", token, {  httpOnly: true, expires: date })
+    return cookie ? true : false
+}
