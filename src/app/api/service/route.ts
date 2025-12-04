813b6a22199e0f2e02914742fdb4d95301ae49af
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-04 13:21:04 -0300

    v1

--- src/app/api/service/route.ts
+++ src/app/api/service/route.ts
@@ -1,15 +1,11 @@
 import { NextResponse } from "next/server";
 import { google } from "@ai-sdk/google";
-import { generateText, zodSchema } from "ai";
+import { generateText } from "ai";
 import type { NextRequest } from "next/server";
 import Prompt from "@/schema/prompt.schema";
 import sanitizeHtml from "sanitize-html";
 import finalPrompt from "@/lib/prompt";
 
-/* TO DO
-1. Git branch or make a v2 API endpoint with text streaming
-2. encode the prompt in TOON format
-*/
 export async function POST(req: NextRequest) {
     try {
         const { prompt }: Prompt = await req.json();
@@ -19,7 +15,7 @@ export async function POST(req: NextRequest) {
             }, { status: 400 })
         }
         let userPrompt = sanitizeHtml(prompt, { allowedAttributes: {}, allowedTags: [] })
-
+       
         const { text } = await generateText({
             model: google('gemini-2.5-pro'),
             prompt: `${finalPrompt.system} ${finalPrompt.user(userPrompt)}`
