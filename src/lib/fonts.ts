6ae3ab401cab6c8854facea04e301ad1e133ca1e
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-04 11:05:01 -0300

    added nunito sans font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Geist, Montserrat, Raleway } from 'next/font/google'
+import { Geist, Montserrat, Raleway, Ubuntu_Sans_Mono, Nunito_Sans } from 'next/font/google'
 
 export const geist = Geist({
     weight: "800",
@@ -13,6 +13,17 @@ export const montserrat = Montserrat({
 })
 
 export const raleway = Raleway({
+    weight: "700",
+    subsets: ["latin"],
+    adjustFontFallback: false
+})
+
+export const ubuntu = Ubuntu_Sans_Mono({
+    weight: "600",
+    subsets: ["latin"],
+    adjustFontFallback: false
+})
+export const nunito = Nunito_Sans({
     weight: "700",
     subsets: ["latin"],
     adjustFontFallback: false
