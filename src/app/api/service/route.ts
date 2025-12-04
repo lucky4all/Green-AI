8b4fb2231f8df4ebaab31d60d0c868aabf2f6a38
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-04 19:34:15 -0300

    feat(api): enhance JWT verification and error handling in POST route

--- src/app/api/service/route.ts
+++ src/app/api/service/route.ts
@@ -6,7 +6,10 @@ import Prompt from "@/schema/prompt.schema";
 import sanitizeHtml from "sanitize-html";
 import finalPrompt from "@/lib/prompt";
 import { z } from "zod";    
-
+import { unauthorized } from "next/navigation";
+import { JWTService } from "@/lib/jwt";
+import { cookies } from "next/headers";
+import { UnauthorizedError, ExternalAiError } from "@/lib/customError";
 const PromptSchema = z.object({
     corrected: z.string(),
     observations: z.array(z.string())
@@ -14,6 +17,10 @@ const PromptSchema = z.object({
 
 export async function POST(req: NextRequest) {
     try {
+        const jwt = new JWTService;
+        const token:any = (await cookies()).get("authtoken");
+        jwt.verifyToken(token)
+        if (!jwt || !token) throw new UnauthorizedError("No posee token.")
         const { prompt }: Prompt = await req.json();
         if (!prompt || prompt.trim() === '' ) {
             return NextResponse.json({
@@ -27,10 +34,19 @@ export async function POST(req: NextRequest) {
             schema: PromptSchema,
             prompt: `${finalPrompt.system} ${finalPrompt.user(userPrompt)}`
         })
+        if (!object) {
+            throw new ExternalAiError("La IA no ha respondido")
+        }
         return NextResponse.json({
             response: object
         })
     } catch (error) {
+        if (error instanceof UnauthorizedError) {
+            unauthorized()
+        }
+        if (error instanceof ExternalAiError) {
+            return NextResponse.json({ error: "La IA no ha podido responder" }, { status: 500 })
+        }
         console.error("Error en la ruta /api/service:", error);
         return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
     }
