6f000c268d7a5b6a40a9fe58b56a6ce0bc7dfa80
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-05 12:53:50 -0300

    Created internal and client errors

--- src/lib/customError.ts
+++ src/lib/customError.ts
@@ -9,3 +9,15 @@ export class ExternalAiError extends Error {
         super(message)
     }
 }
+
+export class InternalServerError extends Error {
+    constructor(message: string) {
+        super(message)
+    }
+}
+
+export class ClientError extends Error {
+    constructor(message: string) {
+        super(message)
+    }
+}
