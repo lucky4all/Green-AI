54319d2e14d0f26bcf94c51812dda5d274e282c6
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-05 12:53:18 -0300

    Removed spinner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { DefaultButton, DocsButton, GitHubButton } from "@/components/buttons"
 import { geist, raleway } from "@/lib/fonts";
 import { Spoiler } from "spoiled";
-import Spinner from "@/components/spinner";
 import { Suspense } from "react";
 
 export default function Home() {
@@ -44,7 +43,7 @@ export default function Home() {
           <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
             <div className="sm:flex sm:items-center sm:justify-between">
               <div className="flex justify-center text-teal-600 sm:justify-start">
-                <Suspense fallback={<Spinner />}>
+                <Suspense fallback={<p>Cargando...</p>}>
                   <GitHubButton message="Ver en GitHub" />
                 </Suspense>
                 
--- src/components/spinner.tsx
+++
@@ -1,5 +0,0 @@
-export default function Spinner() {
-    return (
-        <div className="w-10 h-10 border-4 border-t-green-500 border-gray-300 rounded-full animate-spin"></div>
-    )
-}
