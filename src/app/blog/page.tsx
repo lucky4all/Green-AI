c998727b2e6883385ad84a751ffd5db747ccd116
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-05 20:09:52 -0300

    blog building page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,8 @@
+import { geist } from "@/lib/fonts"
 export default async function BlogPage() {
     return (
-        <></>
+        <main className={geist.className}>
+            <h1 className="text-7xl font-bold text-center mt-[10vh]">Pagina en construcción 🍃</h1>
+        </main>
     )
 }
